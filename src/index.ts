b2619ecbf1ce7eef5c4c1f49603b754dd260b957
paulopes1 <paulolopesplj@gmail.com>
2025-02-24 09:16:34 -0300

    Exercício Classe e Instância

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,13 @@ function mensagemPersonalizada(nome: string): string {
     return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
 }
 console.log(mensagemPersonalizada("Ana"));
+
+//Exercício Classe e Instância
+class Carro {
+    constructor(public marca: string, public modelo: string, public ano: number) {}
+    detalhes(): string {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
+}
+const carro1 = new Carro("Toyota", "Corolla", 2022);
+console.log(carro1.detalhes());
