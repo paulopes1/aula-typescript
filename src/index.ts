299e8e70df0c5dff30988d310810239222a86de9
paulopes1 <paulolopesplj@gmail.com>
2025-02-24 09:14:31 -0300

    Exercício de tipagem e arrays

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-let mensagem: string = "Olá, TypeScript!";
-console.log(mensagem);
+
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
+
