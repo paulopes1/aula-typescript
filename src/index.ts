b96a1419fd27cfa99e5302a334e2b52a2d2555b0
paulopes1 <paulolopesplj@gmail.com>
2025-02-24 09:15:48 -0300

    Exercício de função tipada

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-
+// Exercício Tipagem e Arrays
 let valores: number[] = [10, 20, 30];
 let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
 console.log("Soma dos valores:", somaValores);
 
+// Exercício Função Tipada
+function mensagemPersonalizada(nome: string): string {
+    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+}
+console.log(mensagemPersonalizada("Ana"));
